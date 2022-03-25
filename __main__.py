f700c301cd544adfd09229dd9b9354c792e16e41
Amy Ariel <aariel3145@gmail.com>
2022-03-24 22:06:56 -0400

    wrote __main__

--- __main__.py
+++ __main__.py
@@ -1,3 +1,7 @@
+import os
+import sys
+from striprtf.striprtf import rtf_to_text
+
 ### 
 # convert .rtf to .txt
 # forked from https://github.com/joshy/striprtf
@@ -5,18 +9,40 @@
 
 ###
 # Change File Extneion for Files in a Folder
-# from https://stackoverflow.com/questions/16736080/change-the-file-extension-for-files-in-a-folder
+# from https://stackoverflow.com/questions/16736080/change-the-file-extension-for-files-in-a-txt_folder
 ###
 
-import os
-import sys
-folder = 'D:\Dropbox\Creative Projects\Coding\rtf-md-conv\rtf_infile'
-oldType = '.txt'
+rtf_folder = "D:/Dropbox/Creative Projects/Coding/rtf-md-conv/rtf_infile"
+txt_folder = "D:/Dropbox/Creative Projects/Coding/rtf-md-conv/txt_md_outfile"
+oldType = '.rtf'
+midType = '.txt'
 newType = '.md'
 
-for filename in os.listdir(folder):
-    infilename = os.path.join(folder, filename)
+# parse .rtf folder
+for filename in os.listdir(rtf_folder):
+    # figure out rtf infile name
+    infilename = os.path.join(rtf_folder, filename)
     if not os.path.isfile(infilename) : continue
+
+    # open .rtf file to read
+    rtf_file = open(infilename, 'r')
+    # read into string
+    rtf_string = rtf_file.read()
+    # close .rtf file
+    rtf_file.close()
+
+    # convert rtf to text
+    txt_string = rtf_to_text(rtf_string)
+
+    # figure out txt outfile name
+    txtfilename = (txt_folder + "/" + filename[0:len(filename)-4] + midType)
+
+    # save string as .txt file
+    txt_file = open(txtfilename, 'w')
+    txt_file.write(txt_string)
+    txt_file.close()
+
+    # rename from .txt to .md
     oldbase = os.path.splitext(filename)
-    newname = infilename.replace(oldType, newType)
-    output = os.rename(infilename, newname)
+    newname = txtfilename.replace(midType, newType)
+    output = os.rename(txtfilename, newname)
